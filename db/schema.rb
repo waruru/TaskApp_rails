c7c9ebb4668a4bedfdbac16c0ea8a5822ebed113
waruru <c1005486@planet.kanazawa-it.ac.jp>
2022-02-05 04:11:35 +0900

    Boardモデル生成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_04_190218) do
+ActiveRecord::Schema.define(version: 2022_02_04_191017) do
+
+  create_table "boards", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "name", default: "", null: false
+    t.bigint "workspaces_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["workspaces_id"], name: "index_boards_on_workspaces_id"
+  end
 
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -30,4 +38,5 @@ ActiveRecord::Schema.define(version: 2022_02_04_190218) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "boards", "workspaces", column: "workspaces_id"
 end
