816045b02c2bae8cc4010af1cd60f7f9e9686ec5
waruru <c1005486@planet.kanazawa-it.ac.jp>
2022-02-05 07:25:32 +0900

    db:seedに追記

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+unless Workspace.first
+  workspace = Workspace.create(name: 'テストワークスペース')
+  board = workspace.boards.create(name: 'テストボード')
+  task_list = board.task_lists.create(name: 'テストリスト')
+  task = task_list.tasks.create(title: "テストタスク")
+end
