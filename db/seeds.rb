d512b8fcea9ac756676e453910e5567587c522c1
waruru <c1005486@planet.kanazawa-it.ac.jp>
2022-02-05 07:39:41 +0900

    db:seedにUserの項目を追記

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,14 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+unless User.first
+  User.create(
+    email: "test@test.com",
+    password: 'password',
+    password_confirmation: 'password'
+  )
+end
+
 unless Workspace.first
   workspace = Workspace.create(name: 'テストワークスペース')
   board = workspace.boards.create(name: 'テストボード')
