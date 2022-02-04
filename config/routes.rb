d777827426573e776a5b376f09f56b5583184c70
waruru <c1005486@planet.kanazawa-it.ac.jp>
2022-02-05 08:02:40 +0900

    ログイン状態でルーティングを変化するように変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  # Userログイン状態でのルートパス
+  authenticated :user do
+    root "top#index", as: "user_authenticated_root"
+  end
+
+  # User非ログイン状態でのルートパス
   root 'top#index'
 end
