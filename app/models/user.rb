7ae6f62a3f6021c639b998bc9deffc4398a8d69e
waruru <c1005486@planet.kanazawa-it.ac.jp>
2022-02-05 06:49:32 +0900

    UserとWorkspaceの関連付け

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :workspaces, through: :workspace_users
+  has_many :workspace_users
 end
--- app/models/workspace.rb
+++ app/models/workspace.rb
@@ -1,3 +1,5 @@
 class Workspace < ApplicationRecord
+  has_many :users, through: :workspace_users
+  has_many :workspace_users
   has_many :boards
 end
