51f9a9ffbd936139fed985a4ffa2c9f0729e7a7c
waruru <c1005486@planet.kanazawa-it.ac.jp>
2022-02-07 08:01:08 +0900

    ビューの変更

--- app/controllers/user/top_controller.rb
+++ app/controllers/user/top_controller.rb
@@ -2,5 +2,7 @@ class User::TopController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    @workspaces = current_user.workspaces
+    @boards = current_user.boards
   end
 end
