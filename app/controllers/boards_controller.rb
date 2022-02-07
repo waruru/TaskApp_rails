77e05087c82d5266469b11bc07bede92f36f97e2
waruru <c1005486@planet.kanazawa-it.ac.jp>
2022-02-07 23:43:05 +0900

    Board new, create, destroy処理を追加

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -7,8 +7,29 @@ class BoardsController < ApplicationController
   end
 
   def new
+    @board = Board.new
   end
 
   def create
+    board = Board.new(board_params)
+    if board.save
+      redirect_to workspace_path(params[:workspace_id])
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    board = Board.find(params[:id])
+    if board.destroy
+      redirect_to workspace_path(board.workspace_id)
+    else
+      render :new
+    end
+  end
+
+  private
+  def board_params
+    params.require(:board).permit(:name).merge(workspace_id: params[:workspace_id])
   end
 end
