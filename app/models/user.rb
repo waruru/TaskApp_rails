af3dc94f904496a2bcdeb6bd4238919df584b595
waruru <c1005486@planet.kanazawa-it.ac.jp>
2022-02-13 05:12:56 +0900

    User unique_idの前方一致検索メソッドを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,13 @@ class User < ApplicationRecord
   has_many :workspaces, through: :workspace_users
   has_many :board_users
   has_many :boards, through: :board_users
+
+  # unique_id 前方一致検索
+  def self.search_by_unique_id(keywords)
+    users = Array.new
+    keywords.each do |keyword|
+      users += User.where(["unique_id like(?)", "#{keyword}%"])
+    end
+    return users
+  end 
 end
