d6541b32947085e47901a945fdac1a8421e77a2e
waruru <c1005486@planet.kanazawa-it.ac.jp>
2022-02-14 09:01:53 +0900

    ワークスペースに所属しているか確認を行うように修正

--- app/controllers/workspaces_controller.rb
+++ app/controllers/workspaces_controller.rb
@@ -1,5 +1,6 @@
 class WorkspacesController < ApplicationController
   before_action :authenticate_user!
+  before_action :confirmation_workspace, only: [:show, :destroy]
 
   def index
     @workspaces = Workspace.all
@@ -7,10 +8,6 @@ class WorkspacesController < ApplicationController
 
   def show
     @workspace = Workspace.includes(:boards, :users).find(params[:id])
-    unless current_user.workspaces.include?(@workspace)
-      redirect_to root_path
-    end
-
     @boards = @workspace.boards
     @users = @workspace.users
   end
@@ -23,7 +20,7 @@ class WorkspacesController < ApplicationController
     workspace = Workspace.new(workspace_params)
     if workspace.save
       current_user.workspaces << workspace
-      redirect_to root_path
+      redirect_to root_url
     else
       render :new
     end
@@ -31,8 +28,9 @@ class WorkspacesController < ApplicationController
 
   def destroy
     workspace = Workspace.find(params[:id])
+    
     if workspace.destroy
-      redirect_to root_path
+      redirect_to root_url and return
     else
       render :show
     end
@@ -41,5 +39,12 @@ class WorkspacesController < ApplicationController
   private
   def workspace_params
     params.require(:workspace).permit(:name)
+  end
+
+  def confirmation_workspace
+    workspace = Workspace.find(params[:id])
+    unless current_user.workspaces.include?(workspace)
+      redirect_to root_url, alert: "そのワークスペースは存在しません。"
+    end
   end
 end
