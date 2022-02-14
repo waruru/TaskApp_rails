d857a6bc878186f1247a228f4a994f38af987b9f
waruru <c1005486@planet.kanazawa-it.ac.jp>
2022-02-14 13:00:52 +0900

    TaskListコントローラで、ボードに所属しているユーザー以外が変更を行えないように修正

--- app/controllers/task_lists_controller.rb
+++ app/controllers/task_lists_controller.rb
@@ -1,5 +1,6 @@
 class TaskListsController < ApplicationController
   before_action :authenticate_user!
+  before_action :confirmation_board
 
   def new
     @task_list = TaskList.new
@@ -19,12 +20,31 @@ class TaskListsController < ApplicationController
     if task_list.destroy
       redirect_to task_list.board
     else
-      redirect_to redirect_back(fallback_location: root_path)
+      redirect_back fallback_location: root_url
     end
   end
 
   private
   def task_list_params
     params.require(:task_list).permit(:name).merge(board_id: params[:board_id])
+  end
+
+  def confirmation_board
+    if params[:board_id].present?
+      board = Board.find(params[:board_id])
+    elsif params[:id].present?
+      task_list = TaskList.find(params[:id])
+      board = task.board
+    else
+      redirect_to root_url, alert: "エラーが発生しました。" and return
+    end
+
+    unless current_user.boards.include?(board)
+      if current_user.workspaces.include?(board.workspace)
+        redirect_to board.workspace, alert: "ボードに参加していないため変更を行えません。" and return
+      else
+        redirect_to root_url, alert: "そのボードは存在しません。" and return
+      end
+    end
   end
 end
