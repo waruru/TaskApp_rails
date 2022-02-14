9882bc9fd0fc983daa88ad201084899a5c2b2571
waruru <c1005486@planet.kanazawa-it.ac.jp>
2022-02-14 15:01:55 +0900

    Tasksコントローラでリストを確認するように修正

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,6 @@
 class TasksController < ApplicationController
   before_action :authenticate_user!
+  before_action :confirmation_task_list
 
   def new
     @task = Task.new
@@ -17,5 +18,16 @@ class TasksController < ApplicationController
   private
   def task_params
     params.require(:task).permit(:title, :memo).merge(task_list_id: params[:task_list_id])
+  end
+
+  def confirmation_task_list
+    task_list = TaskList.find(params[:task_list_id])
+    unless current_user.boards.include?(task_list.board)
+      if current_user.workspaces.include?(task_list.board.workspace)
+        redirect_to task_list.board.workspace, alert: "ボードに参加していないため変更を行えません。" and return
+      else
+        redirect_to root_url, alert: "そのリストは存在しません。"
+      end
+    end
   end
 end
