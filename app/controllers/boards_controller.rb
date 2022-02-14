6a63503054bead127576986e6870a153097800f9
waruru <c1005486@planet.kanazawa-it.ac.jp>
2022-02-14 15:37:38 +0900

    確認処理を修正

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,7 +1,7 @@
 class BoardsController < ApplicationController
   before_action :authenticate_user!
-  before_action :confirmation_workspace, only: [:new, :create]
-  before_action :confirmation_board, only: [:show, :destroy]
+  before_action :confirmation_workspace, only: [:show, :new, :create]
+  before_action :confirmation_board, only: [:destroy]
 
   def index
   end
@@ -41,13 +41,24 @@ class BoardsController < ApplicationController
 
   def confirmation_board
     board = Board.find(params[:id])
-    unless current_user.workspaces.include?(board.workspace)
-      redirect_to root_url, alert: "そのボードは存在しません。"
+    unless current_user.boards.include?(board)
+      if current_user.workspaces.include?(board.workspace)
+        redirect_to board, alert: "ボードに参加していないため変更を行えません。" and return
+      else
+        redirect_to root_url, alert: "そのボードは存在しません。" and return
+      end
     end
   end
 
   def confirmation_workspace
-    workspace = Workspace.find(params[:workspace_id])
+    if params[:workspace_id].present?
+      workspace = Workspace.find(params[:workspace_id])
+    elsif params[:id].present?
+      board = Board.find(params[:id])
+      workspace = board.workspace
+    else
+      redirect_to root_url, alert: "エラーが発生しました。"
+    end
     unless current_user.workspaces.include?(workspace)
       redirect_to root_url, alert: "そのワークスペースは存在しません。"
     end
