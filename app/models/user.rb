5c027f8534a8a2dff3b3f42050809bd46b864eb1
waruru <c1005486@planet.kanazawa-it.ac.jp>
2022-02-15 14:47:55 +0900

    Userモデルに関連付けを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,7 @@ class User < ApplicationRecord
   has_many :workspace_users
   has_many :workspaces, through: :workspace_users
   has_many :workspace_join_requests, class_name: "WorkspaceJoinRequest", foreign_key: "request_recipient_id"
+  has_many :join_request_workspaces, through: :workspace_join_requests, source: :workspace
   has_many :board_users
   has_many :boards, through: :board_users
 
