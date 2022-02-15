8980c81268474d53b72a9775454d7830b5e33caa
waruru <c1005486@planet.kanazawa-it.ac.jp>
2022-02-15 10:13:24 +0900

    WorkspaceJoinRequestモデル側の関連付けを設定

--- app/models/workspace_join_request.rb
+++ app/models/workspace_join_request.rb
@@ -1,2 +1,5 @@
 class WorkspaceJoinRequest < ApplicationRecord
+  belongs_to :workspace
+  belongs_to :sender, class_name: "User", foreign_key: "request_sender_id"
+  belongs_to :recipient, class_name: "User", foreign_key: "request_recipient_id"
 end
