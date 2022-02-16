e55cd5b8411df8880a06cfc62522237bc44ad94c
waruru <c1005486@planet.kanazawa-it.ac.jp>
2022-02-16 18:18:15 +0900

    ワークスペース参加時に招待を削除

--- app/controllers/workspace_users_controller.rb
+++ app/controllers/workspace_users_controller.rb
@@ -4,16 +4,19 @@ class WorkspaceUsersController < ApplicationController
 
   def create
     user = User.find(params[:user])
-    workspace = Workspace.includes(:users, :join_request_recipients).find(params[:workspace_id])
+    workspace = Workspace.includes(:users, :join_requests).find(params[:workspace_id])
+    request = workspace.join_requests.find_by(recipient: user)
+
     if workspace.users.include?(user)
       flash[:alert] = "そのユーザーはすでに所属しています。"
       redirect_to root_url
-    elsif workspace.join_request_recipients.exclude?(user)
+    elsif not workspace.join_requests.exists?(recipient: user)
       flash[:alert] = "ワークスペースに招待されていません。"
       redirect_to root_url
     else
       workspace_user = WorkspaceUser.new(user: user, workspace: workspace)
       if workspace_user.save
+        request.destroy
         redirect_to workspace, notice: "#{workspace.name} に参加しました。"
       else
         flash[:alert] = "参加に失敗しました。"
