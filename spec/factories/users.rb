1a70d06837e5537de7e9873a50916d96ed69a9fa
waruru <c1005486@planet.kanazawa-it.ac.jp>
2022-02-21 21:17:22 +0900

    見やすいようにインデントを調整

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :user do
     sequence(:unique_id) { |n| "tester#{n}" }
-    sequence(:email) { |n| "tester#{n}@example.com" }
-    password { "password" }
+    sequence(:email)     { |n| "tester#{n}@example.com" }
+    password             { "password" }
   end
 end
