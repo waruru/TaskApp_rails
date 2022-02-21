5b14345e6a435952ad51f7fc447cd712a6bdabdb
waruru <c1005486@planet.kanazawa-it.ac.jp>
2022-02-21 15:47:08 +0900

    アプリのデフォルト言語を日本語に設定

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,10 @@ module TaskApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
-
+    # 言語の設定
+    config.i18n.default_locale = :ja
+    # 言語ファイルの読み込み設定
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.yml').to_s]
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
