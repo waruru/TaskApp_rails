491352331e8faeedb269cd113e36577fd4fa8016
waruru <c1005486@planet.kanazawa-it.ac.jp>
2022-03-03 04:46:10 +0900

    tailwind minWidth オプション追加

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -9,10 +9,16 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans]
       },
       minHeight: {
-        '1/2': '50%',
+        '1/2': '50%'
+      },
+      minWidth: {
+        '1/5': '20%',
+        '2/5': '40%',
+        '3/5': '60%',
+        '4/5': '80%'
       }
     }
   },
